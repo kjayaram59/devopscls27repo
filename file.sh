13e4497f40741292288d8481e9187e43246fb4d6
kjayaram59 <kjayaram59@gmail.com>
2021-12-14 17:16:15 +0530

    add new line for date

--- file.sh
+++ file.sh
@@ -3,4 +3,6 @@ echo "************** DISPLAY MEMORY INFORMATION *****************"
 free -m
 echo "******************DISPLAY ROOT FILE SYSTEM USAGE**************"
 df -h /
+echo "***************DISPLAY TODAY DATE*****************"
+date
 
